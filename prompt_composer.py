0637853724bf59fcefd35aef410878eeea99c95d
mpujanek <m.p.pujanek@student.tue.nl>
2025-04-25 22:49:55 +0200

    Implemented prompt_composer

---
+++ prompt_composer.py
@@ -0,0 +1,12 @@
+
+
+def compose(prompt_filename, tutorial_filename):
+    with open(prompt_filename, "r") as file:
+        generic_prompt = file.read()
+
+    with open(tutorial_filename, "r") as file:
+        tutorial = file.read()
+
+    prompt = generic_prompt + tutorial
+
+    return prompt
