5f13815701666f82d9b6fb8047776b5d51c46c1c
mpujanek <m.p.pujanek@student.tue.nl>
2025-05-05 14:32:56 +0200

    changed shell scripts

--- exercises/check.sh
+++ exercises/check.sh
@@ -4,4 +4,9 @@ coq_with_imports() {
     cat "$imports" "$1" > "$tmpfile" && coqc -w -all "$tmpfile" && rm "$tmpfile"
 }
 
+if [[ -z "$1" ]]; then
+    echo "Usage: $0 file.v"
+    exit 1
+fi
+
 coq_with_imports "$1"
