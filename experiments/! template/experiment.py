6b49c0a6e46fb89053346ea787ec951f05d20dba
Mikolaj Pujanek <m.p.pujanek@student.tue.nl>
2025-06-15 03:09:56 +0200

    fixed and reran exercises

--- experiments/! template/experiment.py
+++ experiments/! template/experiment.py
@@ -15,6 +15,22 @@ models = {"openai/gpt-4.1": 0,
 
 exercise_numbers = ["10_7_6", "10_7_intermediate"]
 
+all_exercises = [
+    "2_non_degenerate_1", "2_non_degenerate_2",
+    "2_positive_1", "2_positive_2",
+    "2_reflexive_1", "2_reflexive_2",
+    "2_symmetric_1", "2_symmetric_2",
+    "2_triangle_inequality_1", "2_triangle_inequality_2",
+    "3_11_1", "3_11_2", "3_11_4",
+    "4_9_1", "4_9_2", "4_9_3",
+    "5_9_1", "5_9_2",
+    "6_8_1", "6_8_2",
+    "10_7_3",
+    "10_7_6", "10_7_intermediate",
+    "11_not_closed", "11_not_open",
+    "13_11_2", "13_11_3"
+]
+
 # provide list of generic definitions in prompt?
 defs_no_context = False
 
--- experiments/fixing_exercises/experiment.py
+++ experiments/fixing_exercises/experiment.py
@@ -3,17 +3,35 @@ from main import run
 
 models = {"openai/gpt-4.1": 0, 
           "openai/o4-mini": 0,
-          "anthropic/claude-sonnet-4": 10, 
+          "anthropic/claude-sonnet-4": 0, 
           "anthropic/claude-3.7-sonnet:thinking": 0,
           "google/gemini-2.5-flash-preview-05-20": 0, 
           "google/gemini-2.5-flash-preview-05-20:thinking": 0,
           "deepseek/deepseek-chat-v3-0324": 0, 
           "deepseek/deepseek-r1-0528": 0,
-          "x-ai/grok-3-mini-beta": 0,
+          "x-ai/grok-3-mini-beta": 50,
           "x-ai/grok-3-beta": 0
           }
 
-exercise_numbers = ["5_9_2"]
+exercise_numbers = [
+    "2_triangle_inequality_2"
+    ]
+
+all_exercises = [
+    "2_non_degenerate_1", "2_non_degenerate_2",
+    "2_positive_1", "2_positive_2",
+    "2_reflexive_1", "2_reflexive_2",
+    "2_symmetric_1", "2_symmetric_2",
+    "2_triangle_inequality_1", "2_triangle_inequality_2",
+    "3_11_1", "3_11_2", "3_11_4",
+    "4_9_1", "4_9_2", "4_9_3",
+    "5_9_1", "5_9_2",
+    "6_8_1", "6_8_2",
+    "10_7_3",
+    "10_7_6", "10_7_intermediate",
+    "11_not_closed", "11_not_open",
+    "13_11_2", "13_11_3"
+]
 
 # provide list of generic definitions in prompt?
 defs_no_context = False
